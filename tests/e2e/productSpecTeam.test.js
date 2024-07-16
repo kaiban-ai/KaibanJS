df4e2d75e6c041b30082ae40e3d786b60b98deb2
Dariel <darielnoel@gmail.com>
2024-07-16 10:55:04 -0400

    Adding basic test suite and ci github actions.

--- tests/e2e/productSpecTeam.test.js
+++ tests/e2e/productSpecTeam.test.js
@@ -1,85 +1,86 @@
 require('dotenv').config({ path: './.env.local' });
-const openAIProducSpecsTeam = require('./examples/teams/openai/productSpecsTeam');
-const geminiAIProducSpecsTeam = require('./examples/teams/google/productSpecsTeam');
-const teamRecordedRequests = require('./examples/teams/openai/productSpecsTeam.requests');
-
-
-
 // Setup mock
 const { mock, record, restoreAll, getRecords } = require('../utils/moscaFetch')();
 
+const openAIProducSpecsTeam = require('./examples/teams/openai/productSpecsTeam');
+const openAITeamRecordedRequests = require('./examples/teams/openai/productSpecsTeam.requests');
+const geminiAIProducSpecsTeam = require('./examples/teams/google/productSpecsTeam');
+const geminiAITeamRecordedRequests = require('./examples/teams/google/productSpecsTeam.requests');
+const anthropicAIProductSpecsTeam = require('./examples/teams/anthropic/productSpecsTeam');
+const anthropicAITeamRecordedRequests = require('./examples/teams/anthropic/productSpecsTeam.requests');
+const mistralAIProductSpecsTeam = require('./examples/teams/mistral/productSpecsTeam');
+const mistralAITeamRecordedRequests = require('./examples/teams/mistral/productSpecsTeam.requests');
 
-
-// Setting up recording
-record({
-    url: '*',
-    method: '*',
-    body: '*'  // Record any POST request to this URL
-});
-
-// Later, after making some requests in your application
-// const recordedData = getRecords();
-// console.log(recordedData);
-
-
-
+// Determine if mocks should be applied based on the environment
+const withMockedApis = process.env.TEST_ENV === 'mocked-llm-apis' ? true : false;
 
 describe('Product Spec Team Workflows', () => {
-    // describe('Using OpenAI Agents', () => {
-    //     beforeEach(() => {         
-    //         // Mocking all POST requests with a callback
-    //         mock(teamRecordedRequests);
-    //     });
-    //     afterEach(() => {
-    //         restoreAll();
-    //     });      
-    //     it('completes the entire workflow successfully', async () => {
-    //         const result = await openAIProducSpecsTeam.start();
-    //         console.log(result);
-    //         expect(result).toBeDefined();
-    //         expect(typeof result).toBe('string');
-    //         const wordCount = result.trim().split(/\s+/).length;
-    //         expect(wordCount).toBeGreaterThan(20);      
-    //     });
-    // });
-
-    describe('Using Gemini AI Agents', () => {
+    describe('Using OpenAI Agents', () => {
         beforeEach(() => {         
             // Mocking all POST requests with a callback
-            // mock(teamRecordedRequests);
+            withMockedApis && mock(openAITeamRecordedRequests);
         });
         afterEach(() => {
-            // restoreAll();
+            withMockedApis && restoreAll();
         });      
         it('completes the entire workflow successfully', async () => {
-
-            debugger;
-            const result = await geminiAIProducSpecsTeam.start();
-            console.log(result);
-            // const recordedData = getRecords();
-            // console.log(recordedData);            
+            const result = await openAIProducSpecsTeam.start();
             expect(result).toBeDefined();
             expect(typeof result).toBe('string');
             const wordCount = result.trim().split(/\s+/).length;
             expect(wordCount).toBeGreaterThan(20);      
         });
-    });    
+    });
 
-    // describe('Using Anthropic Agents', () => {
-    //     it('completes the entire workflow successfully', async () => {
-    //         // Setup for Product Spec Team with Anthropic
-    //         // Execute workflow
-    //         // Assertions and expectations
-    //         expect(true).toBeDefined();
-    //     });
-    // });
+    describe('Using Gemini AI Agents', () => {
+        beforeEach(() => {         
+            // Mocking all POST requests with a callback
+            withMockedApis && mock(geminiAITeamRecordedRequests);
+        });
+        afterEach(() => {
+            withMockedApis && restoreAll();
+        });      
+        it('completes the entire workflow successfully', async () => {
 
-    // describe('Using Gemini Agents', () => {
-    //     it('completes the entire workflow successfully', async () => {
-    //         // Setup for Product Spec Team with Gemini
-    //         // Execute workflow
-    //         // Assertions and expectations
-    //         expect(true).toBeDefined();
-    //     });
-    // });
+            const result = await geminiAIProducSpecsTeam.start();         
+            expect(result).toBeDefined();
+            expect(typeof result).toBe('string');
+            const wordCount = result.trim().split(/\s+/).length;
+            expect(wordCount).toBeGreaterThan(20);      
+        });
+    });
+
+    describe('Using Anthropic Agents', () => {
+        beforeEach(() => {         
+            // Mocking all POST requests with a callback
+            withMockedApis && mock(anthropicAITeamRecordedRequests);
+        });
+        afterEach(() => {
+            withMockedApis && restoreAll();
+        });      
+        it('completes the entire workflow successfully', async () => {
+            const result = await anthropicAIProductSpecsTeam.start();                          
+            expect(result).toBeDefined();
+            expect(typeof result).toBe('string');
+            const wordCount = result.trim().split(/\s+/).length;
+            expect(wordCount).toBeGreaterThan(20);      
+        });
+    });        
+    describe('Using Mistral AI Agents', () => {
+        beforeEach(() => {         
+            // Mocking all POST requests with a callback
+            withMockedApis && mock(mistralAITeamRecordedRequests);
+        });
+        afterEach(() => {
+            withMockedApis && restoreAll();
+        });      
+        it('completes the entire workflow successfully', async () => {
+
+            const result = await mistralAIProductSpecsTeam.start();                      
+            expect(result).toBeDefined();
+            expect(typeof result).toBe('string');
+            const wordCount = result.trim().split(/\s+/).length;
+            expect(wordCount).toBeGreaterThan(20);      
+        });
+    });
 });
