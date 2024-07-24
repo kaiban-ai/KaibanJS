8fcca9b347399f32e4672b749655ead1fe2192f3
Dariel <darielnoel@gmail.com>
2024-07-24 15:58:59 -0400

    Fix external dependencies compilation for NodeJS.

--- rollup.config.js
+++ rollup.config.js
@@ -4,55 +4,47 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import sourcemaps from 'rollup-plugin-sourcemaps';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-// Determine if we are in development mode based on the NODE_ENV environment variable
 const isDevelopment = process.env.NODE_ENV === 'development';
 const isTest = process.env.TEST_ENV === 'mocked-llm-apis';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/bundle.cjs.js',
-      format: 'cjs',
+function generateConfig(format) {
+  
+  const isESM = format === 'es';
+  const external = isESM ? ['react', 'react-dom', 'uuid'] : ['uuid'];
+  return {
+    input: 'src/index.js',
+    output: {
+      file: `dist/bundle.${format === 'cjs' ? 'cjs' : format === 'es' ? 'esm' : 'umd'}.js`,
+      format: format,
       inlineDynamicImports: true,
-      sourcemap: true // Enable sourcemap generation for CommonJS
+      sourcemap: true,
+      name: format === 'umd' ? 'AgenticJS' : undefined
     },
-    {
-      file: 'dist/bundle.esm.js',
-      format: 'es',
-      inlineDynamicImports: true,
-      sourcemap: true // Enable sourcemap generation for ES Module
-    },
-    {
-      file: 'dist/bundle.umd.js',
-      format: 'umd',
-      inlineDynamicImports: true,
-      name: 'AgenticJS',
-      sourcemap: true // Enable sourcemap generation for UMD
-    }
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve({
-      browser: true,  // Set to false to prioritize Node.js modules
-      preferBuiltins: false,  // Ensure Node.js built-ins are used
-      mainFields: ['browser', 'module', 'main']
-    }),
-    commonjs(),
-    // Include the replace plugin only if isTest is true
-    ...(isTest ? [replace({
-      'shims.fetch': `(...args) => { return global.fetch(...args); };\n`, // Include newline
-      preventAssignment: true
-    })] : []),
-    babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**'
-    }),
-    // Only include terser (minification) if not in development mode
-    ...(!isDevelopment ? [terser()] : []),
-    sourcemaps(),  // Ensure sourcemaps from dependencies are handled
-  ],
-  external: ['react', 'react-dom', 'uuid'],
-};
+    external: external,
+    plugins: [
+      resolve({
+        browser: true,
+        preferBuiltins: false,
+        mainFields: ['browser', 'module', 'main']
+      }),
+      commonjs(),
+      ...(isTest ? [replace({
+        'shims.fetch': `(...args) => { return global.fetch(...args); };\n`,
+        preventAssignment: true
+      })] : []),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**'
+      }),
+      ...(!isDevelopment ? [terser()] : []),
+      sourcemaps(),
+    ]
+  };
+}
+
+export default [
+  generateConfig('cjs'),
+  generateConfig('es'),
+  generateConfig('umd')
+];
