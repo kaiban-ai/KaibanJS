821cf6af70df13702f7bb11510fb5d7972c9628f
Dariel <darielnoel@gmail.com>
2024-07-30 20:08:08 -0400

    Adds new Tavily API_KEY

--- playground/react/src/teams/sport_news/openai.js
+++ playground/react/src/teams/sport_news/openai.js
@@ -5,7 +5,8 @@ import { WikipediaQueryRun } from "@langchain/community/tools/wikipedia_query_ru
 // Define tools
 const searchTool = new TavilySearchResults({
     maxResults: 3,
-    apiKey: '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',
+    // apiKey: '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',
+    apiKey: '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',
 });
 
 const wikiTool = new WikipediaQueryRun({
