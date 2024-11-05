bf2293d5ae22f13a5a3ba25fc6cbb9f10bfc3cfb
Abhishek Karmakar <abhi.karma.2007@gmail.com>
2024-11-05 19:37:38 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
       '^kaibanjs$': '<rootDir>/dist/bundle.cjs'
   },
   testTimeout: 300000, // Sets global timeout to 10 seconds for all tests
-  testEnvironment: "node", // Use Node.js environment for executing tests,
+  testEnvironment: 'node', // Use Node.js environment for executing tests,
   verbose: true, // Make Jest more verbose
   silent: false, // Ensure Jest is not silent (though this is not directly related to console.log output)
   // testMatch: [
