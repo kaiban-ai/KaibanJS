657deda547e4d23aeebd803d08141f49117c64e4
Dariel Noel <darielnoel@gmail.com>
2024-11-07 10:32:34 -0500

    Removing deprecated .eslintignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginTs from "typescript-eslint";
-import parserTs from "@typescript-eslint/parser"; // Added parser for TypeScript
+import parserTs from "@typescript-eslint/parser";
 import pluginReact from "eslint-plugin-react";
 import pluginJest from "eslint-plugin-jest";
 
 export default [
+  {
+    ignores: ["node_modules/*", "dist/*"],
+  },
   pluginJs.configs.recommended,
   ...pluginTs.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginJest.configs["flat/recommended"],
   {
-    ignores: ["node_modules", "dist"],
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
